31acbd545dfa7092c4239c9ba16dcc02a67fcedd
rucadi <ruben.cano96@gmail.com>
2026-03-23 22:52:50 +0100

    Add nix building + distribution follow submod

--- flake.nix
+++ flake.nix
@@ -5,19 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     self.submodules = true;
     efsw = {
-      url = "path::src/thirdparty/efsw";
+      url = "git+file:src/thirdparty/efsw";
       flake = false;
     };
     soil2 = {
-      url = "path::src/thirdparty/soil2";
+      url = "git+file:src/thirdparty/soil2";
       flake = false;
     };
     premakeNinja = {
-      url = "path:premake/premake-ninja";
+      url = "git+file:premake/premake-ninja";
       flake = false;
     };
     premakeCMake = {
-      url = "path::premake/premake-cmake";
+      url = "git+file:premake/premake-cmake";
       flake = false;
     };
   };
