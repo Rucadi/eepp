a8d61699cf254fa987230f5668dd587a69ce3a2f
rucadi <ruben.cano96@gmail.com>
2026-03-23 23:12:08 +0100

    Add nix building + distribution

--- flake.nix
+++ flake.nix
@@ -4,26 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     self.submodules = true;
-    efsw = {
-      url = "git+file:./src/thirdparty/efsw";
-      flake = false;
-    };
-    soil2 = {
-      url = "git+file:./src/thirdparty/soil2";
-      flake = false;
-    };
-    premakeNinja = {
-      url = "git+file:./premake/premake-ninja";
-      flake = false;
-    };
-    premakeCMake = {
-      url = "git+file:./premake/premake-cmake";
-      flake = false;
-    };
+
   };
 
   outputs =
-    { self, efsw, soil2, premakeNinja, premakeCMake, nixpkgs }:
+    { self, nixpkgs }:
     let
       systems = [
         "x86_64-linux"
@@ -41,10 +26,6 @@
               eepp_pkgs = (
                 {
                   stdenv,
-                  efsw,
-                  soil2,
-                  premakeNinja,
-                  premakeCMake,
                   ninja,
                   glew,
                   libx11,
@@ -63,7 +44,11 @@
                 stdenv.mkDerivation {
                   pname = "eepp";
                   version = "unstable";
-                  src = ./.;
+                  src = builtins.fetchGit {
+                      url = "https://github.com/SpartanJ/eepp.git";
+                      ref = "develop";
+                      submodules = true;
+                  };
 
                   nativeBuildInputs = [
                     premake5
@@ -78,16 +63,7 @@
                   ];
 
                   configurePhase = ''
-                    rm -rf src/thirdparty/efsw
-                    rm -rf src/thirdparty/SOIL2
-                    cp -rp ${efsw} src/thirdparty/efsw
-                    cp -rp ${soil2} src/thirdparty/SOIL2
-
-                    rm -rf premake/premake-ninja
-                    rm -rf premake/premake-cmake
-                    cp -rp ${premakeNinja} premake/premake-ninja
-                    cp -rp ${premakeCMake} premake/premake-cmake
-
+  
                     premake5 --disable-static-build gmake
                   '';
 
@@ -110,11 +86,7 @@
               );
             in
             {
-              eepp = pkgs.callPackage eepp_pkgs {
-                efsw = efsw;
-                soil2 = soil2;
-                premakeNinja = premakeNinja;
-                premakeCMake = premakeCMake;
+              eepp = pkgs.callPackage eepp_pkgs { 
               };
             };
         }) systems
